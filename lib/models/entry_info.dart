86fbd6a7d324fcf0ff124d710bda680f84486f85
Dimitar Dimitrov <42871796+Whimfoome@users.noreply.github.com>
2022-08-07 19:34:49 +0300

    Add comment to remind how to run build runner

--- lib/models/entry_info.dart
+++ lib/models/entry_info.dart
@@ -2,6 +2,13 @@ import 'package:hive/hive.dart';
 
 part 'entry_info.g.dart';
 
+/*
+  part 'entry_info.g.dart' is from build_runner generated file.
+  If you edit this file, remember to run:
+  `flutter packages pub run build_runner build`
+  to update the generated code.
+*/
+
 @HiveType(typeId: 0)
 class EntryInfo extends HiveObject {
   @HiveField(0)
